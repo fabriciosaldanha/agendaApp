a68f37539ad4ba64a20aae58550448126d21029a
Fabricio Saldanha <Fabricio Saldanha>
2017-01-30 08:53:45 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     Committer: Fabricio Saldanha <Fabricio Saldanha>
    
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	new file:   .idea/vcs.xml
    new file:   app/src/main/java/br/com/alura/agenda/FormularioHelper.java
    new file:   app/src/main/java/br/com/alura/agenda/br/com/alura/agenda/modelo/Aluno.java
    	new file:   app/src/main/res/menu/menu_formulario.xml
    
     Changes not staged for commit:
    	modified:   .idea/misc.xml
    modified:   .idea/modules.xml
    	modified:   app/src/main/AndroidManifest.xml
    	modified:   app/src/main/java/br/com/alura/agenda/FormularioActivity.java
    	modified:   app/src/main/java/br/com/alura/agenda/FormularioHelper.java
    	modified:   app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
    	modified:   app/src/main/java/br/com/alura/agenda/br/com/alura/agenda/modelo/Aluno.java
    	modified:   app/src/main/res/layout/activity_lista_alunos.xml
    	deleted:    app/src/main/res/layout/content_lista_alunos.xml
    	modified:   app/src/main/res/layout/formulario.xml
    	modified:   app/src/main/res/menu/menu_formulario.xml
    
     Untracked files:
    	app/src/main/res/drawable-hdpi/
    	app/src/main/res/drawable-mdpi/
    	app/src/main/res/drawable-xhdpi/
    	app/src/main/res/drawable/

---
+++ app/src/main/java/br/com/alura/agenda/FormularioHelper.java
@@ -0,0 +1,8 @@
+package br.com.alura.agenda;
+
+/**
+ * Created by Fabricio on 29/01/2017.
+ */
+
+public class FormularioHelper {
+}
---
+++ app/src/main/java/br/com/alura/agenda/br/com/alura/agenda/modelo/Aluno.java
@@ -0,0 +1,10 @@
+package br.com.alura.agenda.br.com.alura.agenda.modelo;
+
+/**
+ * Created by Fabricio on 29/01/2017.
+ */
+
+public class Aluno {
+
+
+}
